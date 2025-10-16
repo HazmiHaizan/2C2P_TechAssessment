e6f90c911f06ba570db7e5b4d8fa5de2ee43e5cc
Mohd Hazmi <PC@DESKTOP-NMF44U6>
2025-10-17 07:56:49 +0800

    Add project files.

---
+++ 2C2P_TechAssessment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2C2P_TechAssessment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
