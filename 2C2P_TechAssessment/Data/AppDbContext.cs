b6b14aeba1b84e626a1a58b6769562b3482aadf5
Mohd Hazmi <PC@DESKTOP-NMF44U6>
2025-10-17 08:12:52 +0800

    Added DbContext

---
+++ 2C2P_TechAssessment/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using _2C2P_TechAssessment.Models;
+
+namespace _2C2P_TechAssessment.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TransactionEntity> Transactions { get; set; } = null;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TransactionEntity>()
+                .HasIndex(t => t.TransactionId)
+                .IsUnique(false);
+        }
+    }
+}
