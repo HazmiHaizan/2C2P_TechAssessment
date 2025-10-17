2866e31e9f78de2c6cddba27b8a029766683b5e4
Mohd Hazmi <PC@DESKTOP-NMF44U6>
2025-10-17 08:08:12 +0800

    Created Transaction Models

---
+++ 2C2P_TechAssessment/Models/TransactionDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace _2C2P_TechAssessment.Models
+{
+    public class TransactionDTO
+    {
+        public string Id { get; set; } = null;
+
+        public string Payment { get; set; } = null;
+
+        public string Status { get; set; } = null;
+    }
+}
---
+++ 2C2P_TechAssessment/Models/TransactionEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _2C2P_TechAssessment.Models
+{
+    public class TransactionEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string TransactionId { get; set; }
+
+        [Required]
+        public decimal Amount {  get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string CurrencyCode { get; set; }
+
+        [Required]
+        public DateTime TransactionDate { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Status { get; set; } 
+    }
+}
