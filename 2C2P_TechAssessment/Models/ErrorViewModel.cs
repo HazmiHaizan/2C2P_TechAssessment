14bcbff7dcd7d7f466e3c43d7c7470f882880859
Mohd Hazmi <PC@DESKTOP-NMF44U6>
2025-10-17 09:37:43 +0800

    Remove unrelated files

--- 2C2P_TechAssessment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _2C2P_TechAssessment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
